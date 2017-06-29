e154fc70c32873880294815937d40ed0a0f1f479
jijiboss <maroki.sugimoto@outlook.com>
2017-06-28 22:08:09 -0400

    Created models, updated "admin" and populated Suppliers and Products.

--- daLab/pork01/admin.py
+++ daLab/pork01/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Suppliers, Products, Orders
+#from .models import Suppliers, Products, Orders, ProductsAdmin, OrderAdmin
+
+admin.site.register(Suppliers)
+admin.site.register(Products) #Model must be declared before the AdminModel
+admin.site.register(Orders) #Model must be declared before the AdminModel
+#admin.site.register(Products, ProductsAdmin) #Model must be declared before the AdminModel
+#admin.site.register(Orders, OrderAdmin) #Model must be declared before the AdminModel
