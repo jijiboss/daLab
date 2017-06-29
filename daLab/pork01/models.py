aadb616ac305642fb93932ffb9b52e50169b84d2
jijiboss <maroki.sugimoto@outlook.com>
2017-06-28 22:48:19 -0400

    CSV download option for Orders

--- daLab/pork01/models.py
+++ daLab/pork01/models.py
@@ -47,6 +47,45 @@ class Orders(models.Model):
     def __str__(self):
         return str(self.order_week) + " - " + str(self.order_date) + " - " + str(self.supplier) + " - " + str(self.prod_code_sumitomo) + " - " + str(self.order_qty) + " - " + str(self.fulfill_qty) + " - " + str(self.production_date) + " - " + str(self.trailer_num) + " - " + str(self.port_of_loading) + " - " + str(self.port_of_unlading)
 
+
 class OrdersAdmin(admin.ModelAdmin):
+    #http://blog.endpoint.com/2012/02/dowloading-csv-file-with-from-django.html
+
+    #for the tabular view
     list_display = ('order_week', 'order_date', 'supplier', 'prod_code_sumitomo',
      'order_qty', 'fulfill_qty', 'production_date', 'trailer_num', 'port_of_loading', 'port_of_unlading')
+
+     #For the "download in CSV format" option
+    actions = ['download_csv']
+
+    def download_csv(self, request, queryset):
+        import csv
+        from django.http import HttpResponse
+        import io #Implements the same interface as file but only in memory and not disk
+        #Open a file stream in write to a writer object.
+
+        #This approach will write the file to the disk.
+        #f = open('some.csv', 'wb') #This would open the file in Binary format so if i try to write str then throws an error
+        #f = open('some.csv', 'w')
+        #writer = csv.writer(f)
+
+        #This approach will write to memory and not to disk.
+        f = io.StringIO()
+        writer = csv.writer(f)
+
+        #Wite the column headers
+        writer.writerow(["order_week", "order_date", "supplier", "prod_code_sumitomo", "order_qty", "fulfill_qty", "production_date", "trailer_num", "port_of_loading", "port_of_unlading"])
+        #Write the query results
+        for s in queryset:
+            writer.writerow([s.order_week, s.order_date, s.supplier, s.prod_code_sumitomo, s.order_qty, s.fulfill_qty, s.production_date, s.trailer_num, s.port_of_loading, s.port_of_unlading])
+
+#        f.close() #This is needed only if using the write-to-disk method (not StringIO)
+        #This prompts the user to open the new CSV file
+#        f = open('some.csv', 'r') #This is needed only if using the write-to-disk method (not StringIO)
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=order-report.csv'
+        return response
+
+    #This adds an entry in the Action drop down on the admin page
+    download_csv.short_description = "Download CSV for selected rows."
